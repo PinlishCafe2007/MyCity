cad22fcae62a595f6444ea9b8032961a0ad8890a
Nikita <balduev07@gmail.com>
2023-05-30 12:01:53 +0700

    page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,51 @@
+<nav>
+    <div class="nav-container">
+        <a href="/" class="nav-logo" title="Back to Homepage">Мой город</a>
+        <div class="nav-links">
+            {#each nav as link}
+                {#if link.isShow}
+                <a href={link.href} class="link">{link.title}</a>
+                {/if}
+            {/each}
+        </div>
+    </div>
+</nav>
+   <div class="container">
+        <!-- Pages will be injected below -->
+        <slot></slot>
+   </div>
+<script>
+    const nav = [
+        {
+            title: `История`,
+            href: `/history`,
+            isShow: true
+        },
+        {
+            title: `Фотографии`,
+            href: `/photo`,
+            isShow: true
+        },
+        {
+            title: `Достопримечательности`,
+            href: `/project`,
+            isShow: true
+        },
+        {
+            title: `Учебные заведения`,
+            href: `/school`,
+            isShow: true
+        },
+        {
+            title: `Отдых`,
+            href: `/chill`,
+            isShow: true
+        },
+    ];
+</script>
+<style>
+    .container {
+    max-width: 1400px;
+    margin: 50px auto;
+    }
+</style>
---
+++ src/routes/school/routes.js
@@ -0,0 +1,12 @@
+export const routes = [
+    {
+        id: 1,
+        title: 'First',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis dignissimos dolorem eaex maiores minima minus nisiperspiciatis sapiente voluptate',
+    },
+    {
+        id: 2,
+        title: 'Second',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis dignissimos dolorem eaex maiores minima minus nisiperspiciatis sapiente voluptate',
+    },
+];
